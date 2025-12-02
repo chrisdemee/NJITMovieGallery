f84f6a181c5377a34c81e461207b04adae84637c
chrisdemee <526cdemetri@frhsd.com>
2025-12-02 10:06:53 -0500

    three

--- site.js
+++ site.js
@@ -34,6 +34,9 @@ const vue_app = Vue.createApp({
             // This holds your movies.json data.
             movies: [],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+            siteTitle: 'IS219 Gallery (Project 3)',
+            owner: 'chrisdemee',
+            githubRepo: 'https://github.com/chrisdemee/NJITMovieGallery'
          
       }
     },
