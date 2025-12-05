67832a5f484ebf93f39fc2199dc00daf7cedd268
chrisdemee <526cdemetri@frhsd.com>
2025-12-05 10:36:39 -0500

    atarted step 7

--- site.js
+++ site.js
@@ -45,6 +45,35 @@ const vue_app = Vue.createApp({
     },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+                        getMonthText(dateArray) {
+                  const monthNames = [
+                        'January', 'February', 'March', 'April', 'May', 'June',
+                        'July', 'August', 'September', 'October', 'November', 'December'
+                  ];
+                  
+                  const year = dateArray[0];
+                  const month = dateArray[1];
+                  const day = dateArray[2];
+                  
+                  let monthText;
+                  switch(month) {
+                        case 1: monthText = 'January'; break;
+                        case 2: monthText = 'February'; break;
+                        case 3: monthText = 'March'; break;
+                        case 4: monthText = 'April'; break;
+                        case 5: monthText = 'May'; break;
+                        case 6: monthText = 'June'; break;
+                        case 7: monthText = 'July'; break;
+                        case 8: monthText = 'August'; break;
+                        case 9: monthText = 'September'; break;
+                        case 10: monthText = 'October'; break;
+                        case 11: monthText = 'November'; break;
+                        case 12: monthText = 'December'; break;
+                        default: monthText = 'Unknown';
+                  }
+                  
+                  return `${monthText} ${day}, ${year}`;
+            }
       }
 })
 
